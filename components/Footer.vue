3eaffac926270dc767df1631b1243bbf73bbb99d
unknown <oscar.bravo@tismart.com>
2023-10-22 19:12:54 -0500

    changes

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-footer app>
+        <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    name : "Footer"
+}
+</script>
+
+<style></style>
---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app-bar height="64" color="mibancoprimary" fixed app>
+        <img src="https://www.yevo.pe/assets/img/logo_negativo.png" width="56">
+        <v-spacer />
+        <v-btn icon>
+            <v-icon color="white">mdi-menu</v-icon>
+        </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name : "Header"
+}
+</script>
