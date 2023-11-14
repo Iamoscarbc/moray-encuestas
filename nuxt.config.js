61dcee972473ecc182100cc4939f9cb510e4b935
cristianxavier147 <oscarmbravoc@gmail.com>
2023-11-13 20:41:47 -0500

    changes for moray

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - moray-encuestas',
-    title: 'moray-encuestas',
+    titleTemplate: 'Moray',
+    title: 'Moray',
     htmlAttrs: {
       lang: 'en'
     },
